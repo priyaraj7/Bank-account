8f37974f038b7e088c5c65c11e01a1b410110085
Priyaraj7 <suppichala@gmail.com>
2020-02-14 22:09:36 -0800

    format files

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,44 @@
-function BankAccount(name, balance){
-    this.name = name;
-    this.balance = balance;
+function BankAccount(name, balance) {
+  this.name = name;
+  this.balance = balance;
 }
 
+BankAccount.prototype.withdrawal = function(amount) {
+  this.balance = this.balance - amount;
+  return this.balance;
+};
 
-
-BankAccount.prototype.withdrawal = function(amount){
-   
-    this.balance = this.balance - amount;
-    return this.balance;
-}
-
-BankAccount.prototype.depositTwo = function(amount){
-    this.balance = this.balance + amount;
-    return this.balance;
-}
-
-
-
-
-
-
-
-
+BankAccount.prototype.depositTwo = function(amount) {
+  this.balance = this.balance + amount;
+  return this.balance;
+};
 
 // User Interface
-$(document).ready(function(){
-    var account;
-    $("#open-account").click(function(){
-    
-        $(".funds").show();
-        
-        var name = $("#name").val();
-        var initialDeposit = parseInt($("#initial-deposit").val());
-        account = new BankAccount(name, initialDeposit);
-        showBalance(account.balance);
-        $('.initial-form').hide();
-    });
+$(document).ready(function() {
+  var account;
+  $("#open-account").click(function() {
+    $(".funds").show();
 
-    $('#withdraw').click(function() {
-        var withdrawal = parseInt($('#withdrawal-amount').val(), 10); // adding base is a good practice
-        var newAmount = account.withdrawal(withdrawal);
-        showBalance(newAmount);
-    });
+    var name = $("#name").val();
+    var initialDeposit = parseInt($("#initial-deposit").val());
+    account = new BankAccount(name, initialDeposit);
+    showBalance(account.balance);
+    $(".initial-form").hide();
+  });
 
-    $('#deposit').click(function() {
-        var deposit = parseInt($('#deposit-amount').val(), 10); // adding base is a good practice
-        var newAmount = account.depositTwo(deposit);
-        showBalance(newAmount);
-    });
-    
-    function showBalance(amount) {
-        $("#balance").html(amount);
-    }
+  $("#withdraw").click(function() {
+    var withdrawal = parseInt($("#withdrawal-amount").val(), 10); // adding base is a good practice
+    var newAmount = account.withdrawal(withdrawal);
+    showBalance(newAmount);
+  });
 
+  $("#deposit").click(function() {
+    var deposit = parseInt($("#deposit-amount").val(), 10); // adding base is a good practice
+    var newAmount = account.depositTwo(deposit);
+    showBalance(newAmount);
+  });
 
-})
+  function showBalance(amount) {
+    $("#balance").html(amount);
+  }
+});
